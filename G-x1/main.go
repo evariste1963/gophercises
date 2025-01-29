abf1e0a45ffec3cea76f699938c716a446e04799
evariste1963 <evariste@hotmail.co.uk>
2025-01-29 21:01:25 +0000

    some refactoring and tidy up

--- G-x1/main.go
+++ G-x1/main.go
@@ -5,8 +5,8 @@ import (
 	"encoding/csv"
 	"flag"
 	"fmt"
-	"log"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -19,7 +19,7 @@ func main() {
 	file, err := os.Open(*fileName)
 
 	if err != nil {
-		log.Fatal("Error while reading file", err)
+		exit(fmt.Sprintf("Failed to open CSV file: %s\n", *fileName), 1)
 	}
 	defer file.Close()
 
@@ -28,13 +28,13 @@ func main() {
 	questions, err := reader.ReadAll()
 
 	if err != nil {
-		fmt.Println("Error reading questions")
+		exit("Error reading questions", 1)
 	}
 
 	//keep track of number of correct answers
 	correct := 0
 	//display total number of questions
-	fmt.Printf("There are %v questions\n", len(questions))
+	fmt.Printf("There are %d questions\n", len(questions))
 	fmt.Print("Press return key when ready\n")
 	fmt.Scanln()
 
@@ -45,19 +45,18 @@ func main() {
 	//run timer
 	go func() {
 		<-timer.C
-		fmt.Printf("\nTime up! You scored %v out of %v", correct, len(questions))
-		//exit program
-		os.Exit(0)
+		exit(fmt.Sprintf("\nTime up! You scored %d out of %d", correct, len(questions)), 0)
+
 	}()
 	startTime := time.Now()
 	for i, question := range questions {
 		// ask the next question
-		fmt.Printf("Question %v - What is:  %v\n", i+1, question[0])
+		fmt.Printf("Question %d - What is:  %v = ", i+1, strings.TrimSpace(question[0]))
 
 		//wait for answer
 		scanner := bufio.NewScanner(os.Stdin)
 		scanner.Scan()
-		ans := scanner.Text()
+		ans := strings.TrimSpace(scanner.Text())
 
 		//compare input answer to actual answer
 		if ans == question[1] {
@@ -69,5 +68,10 @@ func main() {
 	timer.Stop()
 
 	//return correct answers and elapsed time
-	fmt.Printf("You scored %v out of %v in %.2f seconds\n", correct, len(questions), (time.Since(startTime)).Seconds())
+	fmt.Printf("You scored %d out of %d in %.2f seconds\n", correct, len(questions), (time.Since(startTime)).Seconds())
+}
+
+func exit(msg string, code int) {
+	fmt.Println(msg)
+	os.Exit(code)
 }
